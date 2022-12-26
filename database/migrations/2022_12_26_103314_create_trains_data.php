f79e9af3f7d8a20189ae5fa9aafccd4a644cc4a6
FRA2721 <cimino.francesco36@gmail.com>
2022-12-26 11:44:50 +0100

    migration

---
+++ database/migrations/2022_12_26_103314_create_trains_data.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     * @author FRANCESCO CIMINO
+     */
+    public function up()
+    {
+        Schema::create('trains_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 100);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 9);
+            $table->tinyInteger('number_carriages')->unsigned();
+            $table->tinyInteger('in_time')->default(1); //(true value--->boolean)
+            $table->tinyInteger('deleted')->default(0); //(false value--->boolean)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     * @author FRANCESCO CIMINO
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains_data');
+    }
+};
